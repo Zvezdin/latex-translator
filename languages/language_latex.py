49750d2b7e1938f832ef61df257218ec937f4f42
zvezdin <git@zvezd.in>
2018-03-02 15:38:40 +0200

    LaTeX quotes + Fix no replace when a key is in another's replacement.

--- languages/language_latex.py
+++ languages/language_latex.py
@@ -14,6 +14,9 @@ class LanguageLaTeX(Language):
 		#note - this also includes \textbf, \caption and similar, which means that currently there is no translation for them. This will change in the future.
 		#note2 - may cause issies with commands like \$\[\]
 		self.command = r'(?s)(\\[^0-9[\]{}\\ ]+)+(({([^{}]|(?2))*})|(\[.*?\]))*'
+		#matches latex quotes
+		self.Lquote = r"``"
+		self.Rquote = r"''"
 		#used to match environment pairs. Also works with nested environments.
 		self.environment = r'(?s)\\begin{(?P<env>.*?)}(.|(?R))*?\\end{(?P=env)}'
 		#used to match math commands. Works with math environments like '$x=3$', '$$x=3$$' and ignores the command '\$'
@@ -29,9 +32,12 @@ class LanguageLaTeX(Language):
 		if self.html:
 			text = '<!DOCTYPE html>\n' + text #make it sound like this is html
 
-
+		#comments
 		text = self.replaceAllRegex(self.comment, text, self.hashText)
-		
+		#quotes
+		text = self.replaceAllRegex(self.Lquote, text, self.hashText)
+		text = self.replaceAllRegex(self.Rquote, text, self.hashText)
+
 		#abstract, begin and enumerate special environments
 		for env in self.specialEnvironments:
 			name = ''
@@ -60,12 +66,30 @@ class LanguageLaTeX(Language):
 		return text
 
 	def decode(self, text):
-		for key in self.replaceDict:
-			if key.lower() in text:
-				replaceWith = self.replaceDict[key].replace('\\', '\\\\') #replace a single '\' char to two. Regex breaks otherwise.
-				text = self.replaceAllRegexText('(?i)' + key, text, replaceWith)
-			else:
-				print("Error- key not in text!", key)
-				print("You'll have to replace it yourself")
-				print(self.replaceDict[key])
+		keys = list(self.replaceDict.keys())
+
+		missingDict = self.replaceDict.copy() # the presence of a key in this dict will mean that
+		#the key was not able to be replaced and is therefore missing
+
+		while True:
+			replacedSomething = False #if we have replaced something, make sure to also iterate all of our lastly missing keys
+			#to see if they are found in the newly replaced text
+			for key in keys:
+				if key.lower() in text:
+					replaceWith = self.replaceDict[key].replace('\\', '\\\\') #replace a single '\' char to two. Regex breaks otherwise.
+					text = self.replaceAllRegexText('(?i)' + key, text, replaceWith)
+
+					replacedSomething = True
+				else:
+					missingDict.pop(key, None)
+			if not replacedSomething:
+				break
+
+		for key in self.htmlWrapper: #clear any left HTML tags
+			text = self.replaceAllRegexText('(?i)' + key, text, '')
+
+		for key in missingDict:
+			print("Error- key not in text!", key)
+			print("You'll have to replace it yourself")
+			print(self.replaceDict[key])
 		return text
